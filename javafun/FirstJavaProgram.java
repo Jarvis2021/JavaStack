2896166d6ee8e996f4141bce639c9ed5fcb32716
pvoola <pvoola@mdsol.com>
2020-06-10 04:53:29 -0400

    pushing myfirstjava program

---
+++ javafun/FirstJavaProgram.java
@@ -0,0 +1,15 @@
+
+public class FirstJavaProgram{
+  public static void main(String[] args){
+
+    String name = "My name is Coding Dojo";
+    int age = 100;
+    String hometown = "My hometown is Burbank, CA";
+
+    System.out.println(name);
+    System.out.printf("I am %d years old %n", age);
+    System.out.println(hometown);
+
+
+  }
+}
